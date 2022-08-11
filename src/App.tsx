ac897acce37073200982d22c44d60000c5de950f
Cagil Cinar <sorcnr@gmail.com>
2022-08-11 18:41:05 +0300

    aws error remove

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Redirect, Route } from 'react-router-dom';
 import {
   IonApp,
+  IonButton,
   IonIcon,
   IonLabel,
   IonRouterOutlet,
   IonTabBar,
   IonTabButton,
   IonTabs,
-  setupIonicReact
+  setupIonicReact,
+  useIonModal
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { ellipse, square, triangle, radio,location,settings } from 'ionicons/icons';
@@ -15,8 +17,11 @@ import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
 import { Client, ConnectionOptions } from 'paho-mqtt'
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
 
-import { Amplify } from "aws-amplify";
+import { Amplify, I18n } from "aws-amplify";
 import { MapView } from '@aws-amplify/ui-react';
 
 
@@ -38,6 +43,8 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
+I18n.setLanguage('tr');
+
 /* Theme variables */
 import './theme/variables.css';
 import React, { createContext, useEffect, useState } from 'react';
@@ -49,10 +56,44 @@ setupIonicReact({
 
 });
 
+Amplify.configure(awsExports);
+
+const Body: React.FC<{
+  count: number;
+  onDismiss: () => void;
+  onIncrement: () => void;
+}> = ({ count, onDismiss, onIncrement }) => (
+  <div>
+    count: {count}
+    <IonButton expand="block" onClick={() => onIncrement()}>
+      Increment Count
+    </IonButton>
+    <IonButton expand="block" onClick={() => onDismiss()}>
+      Close
+    </IonButton>
+  </div>
+);
+
 
 
 
 const App: React.FC = () => {
+
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDismiss = () => {
+    dismiss();
+  };
+  
+  const [present, dismiss] = useIonModal(Body, {
+    count,
+    onDismiss: handleDismiss,
+    onIncrement: handleIncrement,
+  });
   
   var serverUrl = "ws://15.188.112.229:8083/mqtt";
   const clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8);
@@ -73,7 +114,7 @@ const App: React.FC = () => {
   const AutoContext = createContext(client);
   useEffect(()=>{
     if (client.isConnected() == false) {
-      client.connect(options);
+      //client.connect(options);
       
       client.onMessageArrived = (message)=>{
         console.log(message.payloadString);
@@ -123,7 +164,7 @@ const App: React.FC = () => {
               <Redirect to="/tab1" />
             </Route>
           </IonRouterOutlet>
-          <IonTabBar slot="bottom">
+            <IonTabBar slot="bottom" >
             <IonTabButton tab="tab1" href="/tab1">
               <IonIcon icon={radio} />
               <IonLabel>Kontrol</IonLabel>
@@ -132,7 +173,7 @@ const App: React.FC = () => {
               <IonIcon icon={location} />
               <IonLabel>Harita</IonLabel>
             </IonTabButton>
-            <IonTabButton tab="tab3" href="/tab3">
+              <IonTabButton tab="tab3" href="/tab3" >
               <IonIcon icon={settings} />
               <IonLabel>Ayarlar</IonLabel>
             </IonTabButton>
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,8 +1,30 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-
+import { IonContent, IonHeader, IonItem, IonLabel, IonList, IonModal, IonPage, IonTitle, IonToolbar, useIonModal } from '@ionic/react';
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import './Tab3.css';
 
+import awsExports from '../aws-exports';
+Amplify.configure(awsExports);
+
+const Login: React.FC = () =>{
+  return (
+    <div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello { user? user.getUsername() : "yabancı"}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+    </div>
+  )
+}
+
 const Tab3: React.FC = () => {
+  const [present,dismiss] = useIonModal(Login);
+
   return (
     <IonPage>
       <IonHeader>
@@ -11,11 +33,17 @@ const Tab3: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+        <IonList>
+          <IonItem onClick={()=>{
+            present({
+              canDismiss:true,
+              showBackdrop:true,
+              animated:true
+            });
+          }}>
+            <IonLabel>Giriş</IonLabel>
+          </IonItem>
+        </IonList>
         
       </IonContent>
     </IonPage>
